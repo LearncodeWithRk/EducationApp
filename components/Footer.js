27c924bead8734e4a1b3ec1bcf70bde9c42141c2
Rohit || LearnCode With RK <71060268+LearncodeWithRk@users.noreply.github.com>
2023-09-29 14:21:48 +0530

    Add existing file

--- components/Footer.js
+++ components/Footer.js
@@ -118,7 +118,7 @@ function Footer() {
 
         <hr className="mt-16 mb-10 border-gray-200" />
 
-        <p className="text-sm text-center text-gray-600">© Copyright 2021, All Rights Reserved by Postcraft</p>
+        <p className="text-sm text-center text-gray-600">© Copyright 2021, All Rights Reserved by RK</p>
     </div>
 </section>
 
